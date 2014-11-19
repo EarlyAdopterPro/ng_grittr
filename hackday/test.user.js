dd7be29a35df0b50450ea7ce15756b972b0c6d7b
halkazar @ aws ec2 <earlyadopter.pro@gmail.com>
2014-11-19 23:57:13 +0000

    hack test

---
+++ hackday/test.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        LSSHack
+// @namespace   A9
+// @include     http://www.amazon.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+alert('testing Hack');
+document.getElementByClass('s9_title').innerHTML = 'test';
